07351aa6d4b6c16202e43a737239abfc68ea22ad
fmendes75 <fmendes75@gmail.com>
2014-08-28 17:12:34 +0100

    Added firmware folder
    This Arduino Sketch test the Nokia LCD sending a simple “hello message”
    and creat a sinusoidal fade in fade out effect on backlight.

---
+++ firmware/test-Nokia_LCD/Test_Nokia_LCD/Test_Nokia_LCD.ino
@@ -0,0 +1,102 @@
+/*
+ * PCD8544 - Interface with Philips PCD8544 (or compatible) LCDs.
+ *
+ * Copyright (c) 2010 Carlos Rodrigues <cefrodrigues@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
+ * To use this sketch, connect the eight pins from your LCD like thus:
+ *
+ * Pin 1 -> +3.3V (rightmost, when facing the display head-on)
+ * Pin 2 -> Arduino digital pin 3
+ * Pin 3 -> Arduino digital pin 4
+ * Pin 4 -> Arduino digital pin 5
+ * Pin 5 -> Arduino digital pin 7
+ * Pin 6 -> Ground
+ * Pin 7 -> 10uF capacitor -> Ground
+ * Pin 8 -> Arduino digital pin 6
+ *
+ * Since these LCDs are +3.3V devices, you have to add extra components to
+ * connect it to the digital pins of the Arduino (not necessary if you are
+ * using a 3.3V variant of the Arduino, such as Sparkfun's Arduino Pro).
+ */
+
+
+#include <PCD8544.h>
+//#include "Waveforms.h"
+#include "arduino.h"
+
+
+// A custom glyph (a smiley)...
+static const byte glyph[] = { B00010000, B00110100, B00110000, B00110100, B00010000 };
+int led = 13;
+
+int backlight = 11;
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+static PCD8544 lcd;
+
+
+void setup() {
+  pinMode(backlight, OUTPUT);
+    
+  pinMode(led, OUTPUT);
+  // PCD8544-compatible displays may have a different resolution...
+  lcd.begin(84, 48);
+  
+  // Add the smiley to position "0" of the ASCII table...
+  lcd.createChar(0, glyph);
+}
+
+
+void loop() {
+  // Just to show the program is alive...
+  static int counter = 0;
+
+  /*digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+*/
+  // Write a piece of text on the first line...
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, World!");
+
+  // Write the counter on the second line...
+  lcd.setCursor(0, 1);
+  lcd.print(counter, DEC);
+  lcd.write(' ');
+  lcd.write(0);  // write the smiley
+
+  delay(30);  
+  counter++;
+
+  analogWrite(backlight, brightness);
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ;
+  }
+}
+
+
+/* EOF - HelloWorld.ino */
